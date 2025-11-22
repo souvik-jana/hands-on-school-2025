aefa1fe1aa84678dae511bf88c978d24bcd75459
Samson Leong (wkstn) <samson.leong@link.cuhk.edu.hk>
2025-11-22 12:25:51 +0800

    Create LVK samples directory

--- env_setup.sh
+++ env_setup.sh
@@ -2,11 +2,13 @@
 
 set -ex
 
+PWD=$(pwd)
+
 echo "Creating the Conda environment"
 conda create -n gw-school-2025 python=3.11 numpy matplotlib astropy lalsimulation lalinspiral pesummary 
 conda activate gw-school-2025
 
-echo "Downloading data"
-mkdir data
+echo "Downloading skymap data"
+cd ./lvk_skyloc_samples
 wget https://zenodo.org/records/5546663/files/skymaps.tar.gz
 tar -xvzf skymaps.tar.gz
