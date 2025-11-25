b6be03d95204e76b247a437949c53ef1be4bde54
Samson Leong (wkstn) <samson.leong@link.cuhk.edu.hk>
2025-11-26 02:01:46 +0800

    Update env setup script

--- env_setup.sh
+++ env_setup.sh
@@ -5,10 +5,15 @@ set -ex
 PWD=$(pwd)
 
 echo "Creating the Conda environment"
-conda create -n gw-school-2025 python=3.11 numpy matplotlib astropy lalsimulation lalinspiral h5py pesummary 
+conda create -n gw-school-2025 -c conda-forge python=3.11 numpy matplotlib astropy lalsimulation lalinspiral h5py pesummary 
 conda activate gw-school-2025
 
 echo "Downloading skymap data"
 cd ./lvk_skyloc_samples
-wget https://zenodo.org/records/5546663/files/skymaps.tar.gz
-tar -xvzf skymaps.tar.gz
+if wget -v > /dev/null 2>&1
+then
+    wget https://zenodo.org/records/5546663/files/skymaps.tar.gz
+    tar -xvzf skymaps.tar.gz
+else
+    echo I will do something with cURL
+fi
