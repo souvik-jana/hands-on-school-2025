30c556276bde749471d752cf040399634c162280
Samson Leong (wkstn) <samson.leong@link.cuhk.edu.hk>
2025-11-26 02:27:09 +0800

    Add arguments parsing

--- env_setup.sh
+++ env_setup.sh
@@ -1,11 +1,57 @@
 #!/usr/bin/bash
 
-set -ex
+# Stop execution as soon as any error is raised.
+set -e
 
 PWD=$(pwd)
 
+# The following getopt example obtained from:
+# https://stackoverflow.com/a/14203146
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+TEST=false
+VERBOSE=false
+CONDA_FLAGS=""
+
+help_msg=$(cat <<EOF
+This script will setup the Conda environment for the workshop,
+and download the necessary data.
+
+Usage: ${0} [options]
+
+  -h,   
+        Display this help message.
+  -t,   
+        Test mode, not installing/downloading anything.
+  -v,   
+        Verbose mode, run while printing every command being executing.
+EOF
+)
+
+while getopts "tvh" opt; do
+  case "$opt" in
+    t)  
+        TEST=true
+        CONDA_FLAGS+="-d "
+        echo "Running in test mode"
+        ;;
+    v)  
+        verbose=true
+        set -x
+        ;;
+    h)  
+        echo "${help_msg}"
+        exit 0
+        ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+
 echo "Creating the Conda environment"
-conda create -n gw-school-2025 -c conda-forge python=3.11 numpy matplotlib astropy lalsimulation lalinspiral h5py pesummary 
+conda create -n gw-school-2025 -c conda-forge --solver=libmamba ${CONDA_FLAGS} python=3.11 numpy matplotlib astropy lalsimulation lalinspiral h5py pesummary 
 conda activate gw-school-2025
 
 echo "Downloading skymap data"
